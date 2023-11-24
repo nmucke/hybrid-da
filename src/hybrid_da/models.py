676b380e744cd7bb319df20aa7c9a5e923118ed0
ntm <nmucke@gmail.com>
2023-11-24 20:37:01 +0100

    Changes

--- src/hybrid_da/models.py
+++ src/hybrid_da/models.py
@@ -1,11 +1,6 @@
 import torch
 import yaml
 
-# open yml file
-with open("configs/variable_names.yml", "r") as ymlfile:
-    config = yaml.load(ymlfile, Loader=yaml.FullLoader)
-
-
 class SpatialEncoder(torch.nn.Module):
     def __init__(
         self,
@@ -22,6 +17,22 @@ class SpatialEncoder(torch.nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return 
 
+class CoordinateEncoder(torch.nn.Module):
+    def __init__(
+        self,
+        input_channels: int,
+        output_channels: int,
+        hidden_channels: int,
+    ) -> None:
+        super().__init__()
+
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+        self.hidden_channels = hidden_channels
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return
+
 class ForwardModel(torch.nn.Module):
     def __init__(
         self,
@@ -30,7 +41,7 @@ class ForwardModel(torch.nn.Module):
         super().__init__()
 
         self.variable_config = variable_config
-        
+
 
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
